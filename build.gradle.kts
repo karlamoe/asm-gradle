c4dd329e2689a2ddaf4dcd6ba0bc341a2f6e3ed0
Karlatemp <i@karla.moe>
2026-01-09 16:31:32 +0800

    Fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
-//mavenPublishing {
+plugins {
+    base
+//    id("moe.karla.maven-publishing")
+    alias(libs.plugins.maven.publish)
+}
+
+mavenPublishing {
 //  GOTO: build-logic/src/main/java/buildlogic/PublishingSetupPlugin.java
-//}
+    manuallyPomSetup = true
+}
 
 tasks.register("publishAllPublicationsToRootStageRepository") {
     dependsOn(
